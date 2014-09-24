4bac49e1c05c213492be397029f9b975c1465d46
Andrew Fallows <kaldrenon@gmail.com>
2014-09-24 16:32:26 -0400

    exclude frame options

--- link.rb
+++ link.rb
@@ -1,6 +1,10 @@
 # My Name is Link
 require 'sinatra'
 
+configure :production do
+  set :protection, :except => :frame_options
+end
+
 get '/' do
   "<h1>Seriously, guys, how hard is it?</h1>"
 end
