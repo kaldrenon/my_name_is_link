0461346ce7d022db3971244f7041ec8e422b1e46
Andrew Fallows <kaldrenon@gmail.com>
2014-09-25 09:02:55 -0400

    update analytics for iframe

--- link.rb
+++ link.rb
@@ -34,7 +34,9 @@ get '/' do
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-55162304-1', 'auto');
+    ga('create', 'UA-55162304-1', 'auto', {
+      'allowLinker': true
+    });
     ga('send', 'pageview');
 
   </script>
